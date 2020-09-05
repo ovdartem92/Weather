345e2cd8749afa31dc5e80ad240e63185688a6b7
Artem Ovdienko <ovd.artem92@gmail.com>
2020-09-05 09:56:12 +0300

    beta 1.1

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,16 @@
 package sample;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ResourceBundle;
 
 public class Controller {
 
@@ -39,7 +44,39 @@ public class Controller {
     @FXML
     void initialize() {
         getData.setOnAction(event -> {
-            System.out.println("All work!");
+            String getUserCity = city.getText().trim();
+            if (!getUserCity.equals("")) {
+                String output = getUrlContent("https://api.openweathermap.org/data/2.5/weather?q=" + getUserCity + "&appid=2891f5912041a8a06a04d15e3fb7d11b");
+                if (!output.isEmpty()) {
+                    JSONObject obj = new JSONObject(output);
+                    temp_info.setText("Температура: " + (obj.getJSONObject("main").getDouble("temp") ));
+                    temp_feels.setText("Ощущается: " + obj.getJSONObject("main").getDouble("feels_like"));
+                    temp_max.setText("Максимум: " + obj.getJSONObject("main").getDouble("temp_max"));
+                    temp_min.setText("Минимум: " + obj.getJSONObject("main").getDouble("temp_min"));
+                    temp_push.setText("Давление: " + obj.getJSONObject("main").getDouble("pressure"));
+                }
+                System.out.println(output);
+            }
         });
+    }
+
+    private static String getUrlContent(String urlAdress) {
+        StringBuffer content = new StringBuffer();
+        try {
+            URL url = new URL(urlAdress);
+            URLConnection urlCon = url.openConnection();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(urlCon.getInputStream()));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                content.append(line + "\n");
+            }
+            reader.close();
+        } catch (Exception e) {
+            System.out.println("Такой город был не найден");
+        }
+        return content.toString();
+
     }
 }
